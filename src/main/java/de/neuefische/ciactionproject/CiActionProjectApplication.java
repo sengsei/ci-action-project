4873a791e56c481769933e15366139f9b9aee540
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-02-15 11:10:46 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/ciactionproject/CiActionProjectApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.ciactionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiActionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiActionProjectApplication.class, args);
+    }
+
+}
